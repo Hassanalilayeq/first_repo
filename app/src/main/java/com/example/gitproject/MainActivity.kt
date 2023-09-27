13f865d068199d7f07e1cc001d4732af827f0e33
Hassan <hassan@gmail.com>
2023-09-27 21:50:31 +0430

    toast2 added

--- app/src/main/java/com/example/gitproject/MainActivity.kt
+++ app/src/main/java/com/example/gitproject/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Happy success day to me", Toast.LENGTH_SHORT).show()
 
         Toast.makeText(this, "log number 1", Toast.LENGTH_SHORT).show()
     }
